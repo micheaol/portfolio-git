ded7510f3f9ae88fb4b16785ff7cee3144fcd082
micheaol <micheaol80@gmail.com>
2022-01-15 13:37:34 +0100

    Style the avatar and detail sec

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { GiFamilyHouse } from 'react-icons/gi';
+import { MdOutlineLocationOn, MdEmail } from 'react-icons/md';
+import { FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Profile = () => {
     return (
-        <div>
-            <h1>Michael Oladele</h1>
+        <div className='detail-layout'>
+           <div className='top-detail'>
+           <h1>Michael Oladele</h1>
             <p>"Full-Stack Software Developer with a love for JavaScript. I listen to music and write in my spare time. Open to new opportunities.</p>
             <button>Edit profile</button>
+           </div>
+           <div className='down-detail'>
+                <ul>
+                    <li> <GiFamilyHouse /> Country : Nigeria</li>
+                    <li><MdOutlineLocationOn /> Abuja, Nigeria</li>
+                    <li>< MdEmail /> micheaol80@gmail.com</li>
+                    <li><FaLinkedin /> https://www.linkedin.com/in/micheaol/</li>
+                    <li>< FaTwitter /> @micheaol</li>
+                </ul>
+           </div>
         </div>
     )
 }
