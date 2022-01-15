00cf0d6c5f716aa34cd8036a4df75243fb511b71
micheaol <micheaol80@gmail.com>
2022-01-15 10:02:12 +0100

    Create left nav element with style

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa';
 
 const TopNav = () => {
     return (
         <div className='top-nav'>
             <div className='left-content'>
                 <div className='logo'>
-                    <h1>logo</h1>
+                <FaGithub size={40} color="white"/>
                 </div>
                 <div className='search-bar'>
-                    <h1>search-bar</h1>
+                <form >
+                    <input type="text" placeholder="Search.." name="search"/>
+                </form>
                 </div>
                 <div className='menu-left'>
-                    <h1>Menu content</h1>
+                    <ul>
+                        <li>Pull request</li>
+                        <li>Issues</li>
+                        <li>Marketplace</li>
+                        <li>Explore</li>
+                    </ul>
                 </div>
             </div>
 
