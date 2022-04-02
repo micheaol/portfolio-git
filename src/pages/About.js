e3bff1eb1962ff7a1ef3d1c2d548e38297162280
micheaol <micheaol80@gmail.com>
2022-04-02 19:22:46 +0100

    Completed about page section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { SiDiscord } from 'react-icons/si';
 
 
 const About = () => {
     return (
-        <div className='about-layout'>
-            <h1 className="bg-success">I am About</h1>
+        <div className='about-layout person-sum'>
+             <div className='welcome-msg'>
+                <h1>Hi 👋, I'm Michael Oladele</h1>
+            </div>
+            <div className='social-icons'>
+                <ul>
+                    <li><a href="#">< SiDiscord size={25} color='blue'/></a></li>
+                    <li><a href="#">< SiDiscord size={25} color='blue'/></a></li>
+                    <li><a href="#">< FaTwitter size={25} color='blue' /></a></li>
+                    <li><a href="#"><FaLinkedin size={25} color='blue' /></a></li>
+                </ul>
+            </div>
+           <p>
+           "Hi! My name is Michael, and I'm a full-stack software developer who specializes in using Javascript, React, Redux, and Rails to build software solutions for small businesses."
+           </p>
+            <br/>
+            <p>
+            I know how to work remotely and collaboratively because of the experience I have gained when learning to code at Microverse, an online training school. In this school, I code every day with other developers around the world and have learned the important skills of communication and teamwork. 
+                </p>
+                <br />
+                <p>
+                I also know how to work to tight deadlines because I submit capstone projects every week and make sure I do not go past the deadline. I also know how to learn new languages quickly! I have already created some projects in React, Javascript, and HTML.
+                </p>
+                <br />
+                <p>
+                On top of this, my previous experience as a customer service Lead taught me discipline and how to meet client expectations, work hard, and always put clients first. 
+                </p>
+                <br />
+                <p>
+                Front-end: JavaScript, HTML5, CSS3, Bootstrap, Styled Components, React, Redux.
+                Back-end: Ruby on Rails.
+                </p>
+                <br/>
+                <p>
+                Tools & Methods: Git, GitHub, Heroku, Mobile First, Responsive Desing, Web testing, React testing, JEST library, API REST.
+                </p>
         </div>
     )
 }
